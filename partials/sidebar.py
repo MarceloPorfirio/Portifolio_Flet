75f0402f062bf7abd759a7bb7be5fb8ef8fb8e6d
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2024-08-12 22:45:17 -0300

    add

--- partials/sidebar.py
+++ partials/sidebar.py
@@ -14,9 +14,16 @@ class SidebarHeader(ft.UserControl):
                         alignment=ft.alignment.bottom_right,
                         bgcolor=ft.colors.PRIMARY,
                         small_size=20,
-                    )
-                ]
-            )
+                    ),
+                    ft.Text(value='Marcelo Porfirio',theme_style=ft.TextThemeStyle.BODY_LARGE,color=ft.colors.WHITE),
+                    ft.Text(value='Desenvolvedor Fullstack',theme_style=ft.TextThemeStyle.BODY_MEDIUM,color=ft.colors.WHITE)
+                
+                ],
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+
+            ),
+            padding=ft.padding.symmetric(vertical=20,horizontal=40),
+            alignment=ft.alignment.center,
         )
     
 class SidebarContent(ft.UserControl):
@@ -25,7 +32,32 @@ class SidebarContent(ft.UserControl):
     
 class SidebarFooter(ft.UserControl):
     def build(self):
-        return ft.Container()
+        return ft.Container(
+            padding=ft.padding.symmetric(vertical=10),
+            content=ft.Row(
+                controls=[
+                    ft.IconButton(
+                        content=ft.Image(src='icons/001-instagram.png',height=15,color='white'),
+                        url='https://www.instagram.com/marcelobrys/'
+                    ),
+                    ft.IconButton(
+                        content=ft.Image(src='icons/002-linkedin.png',height=15,color='white'),
+                        url='https://www.linkedin.com/in/marcelo-porf%C3%ADrio-55a198161/'
+                    ),
+                    ft.IconButton(
+                        content=ft.Image(src='icons/003-github.png',height=15,color='white'),
+                        url='https://github.com/MarceloPorfirio'
+                    ),
+                    ft.IconButton(
+                        content=ft.Image(src='icons/004-youtube.png',height=15,color='white'),
+                        url='https://www.instagram.com/marcelobrys/'
+                    ),
+
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                vertical_alignment=ft.CrossAxisAlignment.CENTER
+            )
+        )
     
 
 
@@ -41,5 +73,5 @@ class Sidebar(ft.UserControl):
 
                 ]
             ),
-            bgcolor=ft.colors.BACKGROUND
+            bgcolor=ft.colors.ON_BACKGROUND
         )
